a910127c894a18699ff23d6c15e1ead6718d8796
mkajic20 <mkajic20@foi.hr>
2023-11-03 18:30:57 +0100

    Created Button component

--- Software/smart-charger/src/utils/styles/theme.js
+++ Software/smart-charger/src/utils/styles/theme.js
@@ -7,4 +7,13 @@ export const colors = {
   bgSecondary: "#0481A9",
   textPrimary: "#FFFFFF",
   formPrimary: "#D9D9D9",
+  hover: "#0f556e",
+};
+
+export const breakpoints = {
+  mobileLarge: "min-width: 420px",
+  tabletSmall: "min-width: 576px",
+  tablet: "min-width: 768px",
+  desktopSmall: "min-width: 1024px",
+  desktop: "min-width: 1440px",
 };
