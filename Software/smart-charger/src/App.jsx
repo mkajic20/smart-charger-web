dee6c92a38319d37e9e59997b9edc44f99291153
irosic20 <irosic20@student.foi.hr>
2023-11-04 21:32:16 +0100

    Added styled components and implemented login logic

--- Software/smart-charger/src/App.jsx
+++ Software/smart-charger/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router";
 import Header from "./components/Header/Header";
 import { Register } from "./pages/Register/Register";
-import Login from "./pages/Login/Login";
+import { Login } from "./pages/Login/Login";
 import { Footer } from "./components/Footer/Footer";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoutes/ProtectedRoute";
--- Software/smart-charger/src/pages/Login/Login.jsx
+++ Software/smart-charger/src/pages/Login/Login.jsx
@@ -1,10 +1,88 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useContext, useState } from "react";
+import Section from "../../components/Section/Section";
+import TextField from "../../components/TextField/TextField";
+import Button from "../../components/Button/Button";
+import {
+  Login as LoginWrapper,
+  LoginError,
+  LoginForm,
+  LoginFormField,
+  LoginFormLabel,
+  LoginFormLink,
+  LoginFormText,
+  ButtonWrapper,
+} from "./LoginStyles";
+import { useNavigate } from "react-router";
+import { loginUser as login } from "../../utils/api/users";
+import { AuthContext } from "../../context/AuthContext";
+import { decodeToken } from "react-jwt";
 
-const Login = (props) => {
-  return <div>Login</div>;
+export const Login = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const { setIsLoggedIn, setRole } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async () => {
+    console.log("I am here -----------------", {
+      email: email,
+      password: password,
+    });
+    const res = await login({ email: email, password: password });
+    if (res.success) {
+      console.log("Oh no now i am here----------");
+      localStorage.setItem("jwt", res.token);
+      const jwtData = decodeToken(res.token);
+      setRole(jwtData.roleId);
+      setIsLoggedIn(true);
+    } else {
+      setError(res.message);
+    }
+  };
+
+  return (
+    <LoginWrapper>
+      <Section
+        title="Sign in"
+        subtitle="Please fill in the following fields in order to sign in:"
+      >
+        <LoginForm>
+          <LoginFormField>
+            <LoginFormLabel>Email: </LoginFormLabel>
+            <TextField
+              placeholder="Your email address"
+              changeValue={setEmail}
+            />
+          </LoginFormField>
+          <LoginFormField>
+            <LoginFormLabel>Password:</LoginFormLabel>
+            <TextField
+              placeholder="Your password"
+              changeValue={setPassword}
+              isPassword
+            />
+          </LoginFormField>
+
+          <LoginError>{error}</LoginError>
+          <ButtonWrapper>
+            <Button buttonText="SIGN IN" onClick={loginUser} />
+          </ButtonWrapper>
+          <LoginFormText>
+            Don't have an account? &nbsp;
+            <LoginFormLink
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              -&gt; REGISTER
+            </LoginFormLink>
+          </LoginFormText>
+        </LoginForm>
+      </Section>
+    </LoginWrapper>
+  );
 };
 
-Login.propTypes = {};
-
-export default Login;
+// export default Login;
