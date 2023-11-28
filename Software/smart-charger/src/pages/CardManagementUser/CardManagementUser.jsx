d9baa9ad0e2ce26c99ef80a24a13d217c2964398
lgaljer20 <lgaljer20@foi.hr>
2023-11-28 19:41:04 +0100

    SC-109 Fully implemented card table with mock data

--- Software/smart-charger/src/pages/CardManagementUser/CardManagementUser.jsx
+++ Software/smart-charger/src/pages/CardManagementUser/CardManagementUser.jsx
@@ -1,9 +1,60 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import {
+  CardManagementControl,
+  CardManagementController,
+  CardManagementTitle,
+  CardTable,
+  CardTableBody,
+  CardTableHead,
+  CardTableHeader,
+  CardTableRow,
+  CardTableCell,
+  CardTableCellDelete,
+  CardTableCellDeleteIcon,
+} from "./CardManagementUserStyles";
+import Icon from "../../assets/trash-icon.png";
+import { getAllUsersCards } from "../../utils/api/userCards";
 export const CardManagementUser = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const asyncCall = async () => {
+      const cardData = await getAllUsersCards();
+      setCards(cardData);
+    };
+    asyncCall();
+  }, []);
   return (
     <>
-      <div>CardManagementUser</div>
+      <CardManagementTitle> My cards</CardManagementTitle>
+      <CardManagementController>
+        <CardManagementControl></CardManagementControl>
+      </CardManagementController>
+      <CardTable>
+        <CardTableHead>
+          <CardTableRow>
+            <CardTableHeader>Card Name</CardTableHeader>
+            <CardTableHeader>Active</CardTableHeader>
+            <CardTableHeader></CardTableHeader>
+          </CardTableRow>
+        </CardTableHead>
+        <CardTableBody>
+          {cards.map((card, index) => (
+            <CardTableRow key={index}>
+              <CardTableCell>{card.name}</CardTableCell>
+              <CardTableCell>
+                {card.active ? "Inactive" : "Active"}
+              </CardTableCell>
+              <CardTableCellDelete>
+                <CardTableCellDeleteIcon
+                  src={Icon}
+                  onClick={() => {}}
+                ></CardTableCellDeleteIcon>
+              </CardTableCellDelete>
+            </CardTableRow>
+          ))}
+        </CardTableBody>
+      </CardTable>
     </>
   );
 };
---
+++ Software/smart-charger/src/utils/api/userCards.js
@@ -0,0 +1,11 @@
+import apiPath from "./api";
+
+export const getAllUsersCards = async () => {
+  return [
+    { id: 0, name: "Kartica 1", active: true },
+    { id: 1, name: "Kartica 2", active: false },
+    { id: 2, name: "Kartica 3", active: true },
+    { id: 3, name: "Kartica 4", active: true },
+    { id: 4, name: "Kartica 5", active: true },
+  ];
+};
