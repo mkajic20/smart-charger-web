96830810daaf3befd16ee290cf576be9f502b5b5
mkajic20 <mkajic20@foi.hr>
2023-11-28 18:06:59 +0100

    SC-91 Implemented functions for charger management

--- Software/smart-charger/src/utils/api/chargers.js
+++ Software/smart-charger/src/utils/api/chargers.js
@@ -1,7 +1,62 @@
 import apiPath from "./api";
+import { decodeToken } from "react-jwt";
 
-export const getChargerData = async () => {};
+export const getChargerData = async (page, size, search) => {
+  const jwt = localStorage.getItem("jwt");
+  let path = `${apiPath}/api/admin/chargers?page=${page}&pageSize=${size}`;
 
-export const deleteCharger = async () => {};
+  if (search.trim().length > 0) {
+    path += `&search=${search}`;
+  }
 
-export const createCharger = async () => {};
+  const res = await fetch(path, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+  console.log("get chargers", data);
+  return data;
+};
+
+export const deleteCharger = async (chargerId) => {
+  const jwt = localStorage.getItem("jwt");
+  const res = await fetch(`${apiPath}/api/admin/chargers/${chargerId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+  console.log(res);
+};
+
+export const createCharger = async (name, latitude, longitude) => {
+  const jwt = localStorage.getItem("jwt");
+  try {
+    const jwtData = decodeToken(jwt);
+    const userId = jwtData.userId;
+
+    const charger = {
+      Name: name,
+      Latitude: latitude,
+      Longitude: longitude,
+      CreatorId: userId,
+    };
+
+    const res = await fetch(`${apiPath}/api/admin/chargers`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(charger),
+    });
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
