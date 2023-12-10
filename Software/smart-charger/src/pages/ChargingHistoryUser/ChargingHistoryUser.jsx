58745dfb22bd3e7c997ce7c0bcc5bae38c6f8a09
mkajic20 <mkajic20@foi.hr>
2023-12-10 14:10:57 +0100

    SC-154 Implemented getting chargers from API

--- Software/smart-charger/src/pages/ChargingHistoryUser/ChargingHistoryUser.jsx
+++ Software/smart-charger/src/pages/ChargingHistoryUser/ChargingHistoryUser.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getUserCharges } from "../../utils/api/userHistory";
 
 export const ChargingHistoryUser = () => {
+  const fetchHistory = async () => {
+    const historyData = await getUserCharges(1, 10, "");
+    console.log(historyData);
+    // if (cardData.success) {
+    //   setCards(cardData.cards);
+    //   setPages(cardData.totalPages);
+    // } else {
+    //   setError(cardData.message);
+    // }
+  };
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
   return <div>ChargingHistoryUser</div>;
 };
