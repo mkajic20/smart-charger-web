8e41399aa41348321466822c6d0c9ba42dfb6569
mkajic20 <mkajic20@foi.hr>
2024-01-08 14:09:14 +0100

    SC-214 Implemented getting statistics from the API

--- Software/smart-charger/src/pages/Statistics/Statistics.jsx
+++ Software/smart-charger/src/pages/Statistics/Statistics.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getStatistics } from "../../utils/api/statistics";
 
 export const Statistics = () => {
+  const [error, setError] = useState("");
+
+  const fetchStatistics = async () => {
+    const data = await getStatistics(1, 2023, 12);
+    if (data.success) {
+      console.log(data);
+    } else {
+      setError(data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchStatistics();
+  }, []);
+
   return <div>Statistics</div>;
 };
