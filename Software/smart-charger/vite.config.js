567f20f00782eb03855efbbf919db597363af559
mkajic20 <mkajic20@foi.hr>
2024-01-17 11:42:18 +0100

    Fixed docker

--- Software/smart-charger/vite.config.js
+++ Software/smart-charger/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "esnext",
+    outDir: "dist",
+    assetsDir: "",
+    minify: "terser",
+  },
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+  },
+});
