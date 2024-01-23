085872f92e08ea14567024819c6c66ef808cbab0
mkajic20 <mkajic20@foi.hr>
2024-01-23 19:58:05 +0100

    SC-231 Added Loader to card management for admin

--- Software/smart-charger/src/pages/CardManagementAdmin/CardManagementAdmin.jsx
+++ Software/smart-charger/src/pages/CardManagementAdmin/CardManagementAdmin.jsx
@@ -23,11 +23,13 @@ import {
   TableRow,
   Title,
   PopupButtonWrapper,
+  LoaderWrapper,
 } from "../../utils/styles/generalStyles";
+import { Blocks } from "react-loader-spinner";
 
 export const CardManagementAdmin = () => {
   const [cards, setCards] = useState([]);
-  const [pages, setPages] = useState(null);
+  const [pages, setPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const [searchTerm, setSearchTerm] = useState("");
@@ -35,15 +37,23 @@ export const CardManagementAdmin = () => {
   const [changedCard, setChangedCard] = useState(null);
   const [deletedCard, setDeletedCard] = useState(null);
 
+  const [loading, setLoading] = useState(true);
+
   const [error, setError] = useState("");
 
   const fetchCardData = async () => {
-    const cardData = await getCardData(currentPage, pageSize, searchTerm);
-    if (cardData.success) {
-      setCards(cardData.cards);
-      setPages(cardData.totalPages);
-    } else {
-      setError(cardData.message);
+    setLoading(true);
+
+    try {
+      const cardData = await getCardData(currentPage, pageSize, searchTerm);
+      if (cardData.success) {
+        setCards(cardData.cards);
+        setPages(cardData.totalPages);
+      }
+    } catch (error) {
+      setError(error.message || "An error occurred while fetching data.");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -67,92 +77,107 @@ export const CardManagementAdmin = () => {
 
   return (
     <>
-      <Title>RFID card management</Title>
-      <Controller>
-        <Control></Control>
-        <Control>
-          <Pagination
-            pages={pages}
-            currentPage={currentPage}
-            prevCall={async () => {
-              if (currentPage > 1) {
-                setCurrentPage(currentPage - 1);
-                await fetchCardData();
-              }
-            }}
-            firstCall={async () => {
-              setCurrentPage(1);
-              await fetchCardData();
-            }}
-            nextCall={async () => {
-              if (currentPage < pages) {
-                setCurrentPage(currentPage + 1);
-                await fetchCardData();
-              }
-            }}
-            lastCall={async () => {
-              setCurrentPage(pages);
-              await fetchCardData();
-            }}
-            withSelect
-            onSelectChange={async (size) => {
-              setPageSize(size);
-              setCurrentPage(1);
-              await fetchCardData();
-            }}
-          />
-        </Control>
-        <Control>
-          <Search
-            placeholder="Search"
-            onCancel={() => {
-              setCurrentPage(1);
-              setSearchTerm("");
-            }}
-            search={(term) => {
-              setCurrentPage(1);
-              setSearchTerm(term);
-            }}
-            showCancel={searchTerm.trim().length > 0}
-          />
-        </Control>
-      </Controller>
-
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeader>Owner</TableHeader>
-            <TableHeader>Card Name</TableHeader>
-            <TableHeader>Active</TableHeader>
-            <TableHeader></TableHeader>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cards.map((card, index) => (
-            <TableRow key={index}>
-              <TableCell>
-                {card.user.firstName} {card.user.lastName}
-              </TableCell>
-              <TableCell>{card.name}</TableCell>
-              <TableCellButton
-                onClick={() => {
-                  setChangedCard(card);
+      {error.length === 0 && (
+        <>
+          <Title>RFID card management</Title>
+          <Controller>
+            <Control></Control>
+            <Control>
+              <Pagination
+                pages={pages}
+                currentPage={currentPage}
+                prevCall={async () => {
+                  if (currentPage > 1) {
+                    setCurrentPage(currentPage - 1);
+                  }
                 }}
-              >
-                {card.active ? "Deactivate" : "Activate"}
-              </TableCellButton>
-              <TableCellDelete>
-                <TableCellIcon
-                  src={DeleteIcon}
-                  onClick={() => {
-                    setDeletedCard(card);
-                  }}
-                />
-              </TableCellDelete>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+                firstCall={async () => {
+                  setCurrentPage(1);
+                }}
+                nextCall={async () => {
+                  if (currentPage < pages) {
+                    setCurrentPage(currentPage + 1);
+                  }
+                }}
+                lastCall={async () => {
+                  setCurrentPage(pages);
+                }}
+                withSelect
+                onSelectChange={async (size) => {
+                  setPageSize(size);
+                  setCurrentPage(1);
+                }}
+              />
+            </Control>
+            <Control>
+              <Search
+                placeholder="Search"
+                onCancel={() => {
+                  setCurrentPage(1);
+                  setSearchTerm("");
+                }}
+                search={(term) => {
+                  setCurrentPage(1);
+                  setSearchTerm(term);
+                }}
+                showCancel={searchTerm.trim().length > 0}
+              />
+            </Control>
+          </Controller>
+
+          {loading ? (
+            <LoaderWrapper>
+              <Blocks
+                height="150"
+                width="150"
+                color="#4fa94d"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                visible={true}
+              />
+            </LoaderWrapper>
+          ) : (
+            <>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableHeader>Owner</TableHeader>
+                    <TableHeader>Card Name</TableHeader>
+                    <TableHeader>Active</TableHeader>
+                    <TableHeader></TableHeader>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {cards.map((card, index) => (
+                    <TableRow key={index}>
+                      <TableCell>
+                        {card.user.firstName} {card.user.lastName}
+                      </TableCell>
+                      <TableCell>{card.name}</TableCell>
+                      <TableCellButton
+                        onClick={() => {
+                          setChangedCard(card);
+                        }}
+                      >
+                        {card.active ? "Deactivate" : "Activate"}
+                      </TableCellButton>
+                      <TableCellDelete>
+                        <TableCellIcon
+                          src={DeleteIcon}
+                          onClick={() => {
+                            setDeletedCard(card);
+                          }}
+                        />
+                      </TableCellDelete>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </>
+          )}
+        </>
+      )}
 
       {changedCard !== null && (
         <PopupWindow
@@ -213,17 +238,17 @@ export const CardManagementAdmin = () => {
           title={"There was an error"}
           text={error}
           onClose={async () => {
-            setSearchTerm("");
-            await fetchCardData();
             setError("");
+            setSearchTerm("");
+            setCurrentPage(1);
           }}
         >
           <Button
             buttonText="Close"
             onClick={async () => {
-              setSearchTerm("");
-              await fetchCardData();
               setError("");
+              setSearchTerm("");
+              setCurrentPage(1);
             }}
           />
         </PopupWindow>
