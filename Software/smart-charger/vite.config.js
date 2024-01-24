e67a5873e97a9e029937d0309352f3dfc55e4176
mkajic20 <mkajic20@foi.hr>
2024-01-24 18:53:21 +0100

    Added eslint to project for static code analysis

--- Software/smart-charger/vite.config.js
+++ Software/smart-charger/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      include: ['src/**/*.js', 'src/**/*.jsx'],
+    }),
+  ],
   build: {
-    target: "esnext",
-    outDir: "dist",
-    assetsDir: "",
-    minify: "terser",
+    target: 'esnext',
+    outDir: 'dist',
+    assetsDir: '',
+    minify: 'terser',
   },
   server: {
     port: 5173,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   },
-});
+})
